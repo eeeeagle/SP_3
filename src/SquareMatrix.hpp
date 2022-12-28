e937626e8bda4ed87e294c4176cfda57ceb097e3
eagle <daniil.osockin@gmail.com>
2022-12-28 14:50:06 +0400

    Added multithreading

--- src/SquareMatrix.hpp
+++ src/SquareMatrix.hpp
@@ -1,16 +1,9 @@
 #ifndef SQUARE_MATRIX
 #define SQUARE_MATRIX
 
-#include "check.hpp"
-#include <iostream>
-#include <vector>
-#include <stdexcept>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
+#include "Common.hpp"
 #include <cmath>
-#include <cstring>
+#include <fcntl.h>
 
 using T = double;
 
@@ -20,6 +13,43 @@ class SquareMatrix
 private:
     std::vector<T> data;
     size_t order;
+
+    struct MulMatrixArgs
+    {
+        std::vector<T> matrix_a;
+        std::vector<T> matrix_b;
+        std::vector<T> matrix_res;
+        size_t start;
+        size_t end;
+        size_t order = 0;
+    };
+public:
+    static int threads_num;
+
+private:
+    static void* mul_parallel(void *ptr_args)
+    {
+        auto *args = (struct MulMatrixArgs *) ptr_args;
+        double start_time = get_time();
+
+        T tmp = 0;
+        for(size_t i = args->start; i < args->end; i++)
+        {
+            int count = 0;
+            for(size_t k = 0; k < args->order; k++)
+            {
+                int inc_str = 0;
+                for(size_t j = 0; j < args->order; j++)
+                    tmp += args->matrix_a[i * args->order + inc_str++] * args->matrix_b[j + k * args->order];
+
+                args->matrix_res[i * args->order + count++] = tmp;
+                tmp = 0;
+            }
+        }
+        printf("Time of one thread working: %fs\n", get_time() - start_time);
+        pthread_exit(nullptr);
+    }
+
 public:
     explicit SquareMatrix(const char* filename)
         :
@@ -59,30 +89,72 @@ public:
 
     friend SquareMatrix operator*(const SquareMatrix& a, const SquareMatrix& b)
     {
+        if(threads_num < 1)
+            throw std::runtime_error("Threads must be > 0:\n"
+                                     "threads_num = " + std::to_string(threads_num));
         if(a.order != b.order)
             throw std::logic_error("SquareMatrix order mismatch:\n"
                                    "Order of matrix A = " + std::to_string(a.order) + '\n' +
                                    "Order of matrix B = " + std::to_string(b.order));
 
         SquareMatrix c(a.order);
+        double start_time = get_time();
 
-        for (int i = 0; i < c.order; i++)
+        /*
+        if(threads_num == 1)
         {
-            for (int j = 0; j < c.order; j++)
+            for (int i = 0; i < c.order; i++)
             {
-                for (int k = 0; k < c.order; k++)
-                    c(i, j) += a(i, k) * b(k, j);
+                for (int j = 0; j < c.order; j++)
+                {
+                    for (int k = 0; k < c.order; k++)
+                        c(i, j) += a(i, k) * b(k, j);
+                }
             }
         }
+        else
+        {
+         */
+            std::vector<pthread_t> threads(threads_num);
+            pthread_attr_t attr;
+            struct MulMatrixArgs args;
 
+            pthread_attr_init(&attr);
+
+            args.matrix_a = a.data;
+            args.matrix_b = b.data;
+            args.matrix_res = c.data;
+            args.order = a.order;
+
+            size_t step = a.order / threads_num;
+            size_t pos = 0;
+            for(size_t k = 0; k < threads_num; k++)
+            {
+                args.start = pos;
+                pos += step;
+                args.end = (k == threads_num - 1) ? a.order : pos;
+
+                if (pthread_create(&threads[k], &attr, mul_parallel, (void *)&args) != 0)
+                {
+                    fprintf(stderr, "Error creating thread\n");
+                    exit(-1);
+                }
+            }
+            pthread_attr_destroy(&attr);
+
+            for(size_t k = 0; k < threads_num; k++)
+                pthread_join(threads[k], nullptr);
+        //}
+        std::cout << "All time: " << get_time() - start_time << std::endl;
         return c;
     }
 
     friend std::ostream& operator<< (std::ostream& out, const SquareMatrix& matrix)
     {
-        for(size_t i = 0; i < matrix.order; i++)
+        const size_t demo_size = (matrix.order > 10) ? 10 : matrix.order;
+        for(size_t i = 0; i < demo_size; i++)
         {
-            for (size_t j = 0; j < matrix.order; j++)
+            for (size_t j = 0; j < demo_size; j++)
                 out << matrix(i, j) << ' ';
 
             out << '\n';
@@ -95,7 +167,7 @@ public:
         struct stat statistic{};
         stat(filename, &statistic);
 
-        order = static_cast<size_t>(sqrt(statistic.st_size / sizeof(T)));
+        order = static_cast<size_t>(sqrt(static_cast<double>(statistic.st_size) / static_cast<double>(sizeof(T))));
         data.resize(order * order);
 
         int fp = check(open(filename, O_RDONLY));
@@ -105,16 +177,9 @@ public:
         check(read(fp, data.data(), statistic.st_size));
         close(fp);
     }
-
-    void write_file(const char* filename) const
-    {
-        int fp = check(open(filename, O_CREAT | O_WRONLY));
-        if (!fp)
-            throw std::invalid_argument("Error opening file: " + std::string(filename));
-
-        check(write(fp, data.data(), data.size() * sizeof(T)));
-        close(fp);
-    }
 };
+
+template <typename T>
+int SquareMatrix<T>::threads_num = 1;
 
 #endif
